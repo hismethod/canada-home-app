6f4d87634b5b82b918e0aa000a88d149255dd36b
Londy <his.methods@gmail.com>
2017-01-02 14:28:13 -0500

    commit all

--- routes/api.js
+++ routes/api.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/post/add', function (req, res) {
+    res.end();
+});
+
 module.exports = router;

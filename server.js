a08da926dcde319fdc4a0fa679e0fd73bba9fa23
Londy <his.methods@gmail.com>
2017-01-03 16:17:56 -0500

    gulp

--- server.js
+++ server.js
@@ -6,20 +6,22 @@ var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
 var mongoose     = require('mongoose');
 
-var index = require('./routes/index');
-var api = require('./routes/api');
+// route
+var index        = require('./routes/index');
+var api          = require('./routes/api');
 
-var app = express();
+// config files
+var db           = require('./config/db');
 
-// view engine setup
+var app          = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -43,14 +45,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-var db = mongoose.connection;
-db.on('error', console.error);
-db.once('open', function () {
-   console.log('Connected to mongodb server');
-});
-
-mongoose.connect('mongodb://admin:***REMOVED***@ds151108.mlab.com:51108/heroku_z98z29mf');
-
 
 module.exports = app;
